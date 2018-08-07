0d3d3ddc3f8c9fbde83eacf152255dfebc9e4299
Anatoliy Zaslavskiy <toli@hover.io>
2018-08-07 12:55:33 -0700

    CRA

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">PickleJS</h1>
+        </header>
+        <p className="App-intro">
+          Cucumber with Brine
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
