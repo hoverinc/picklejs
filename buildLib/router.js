e641843ab3db7154d3a5c6c169ba421ecea68110
Anatoliy Zaslavskiy <toli@hover.io>
2018-08-14 14:24:16 -0700

    v0.20.0

--- buildLib/router.js
+++ buildLib/router.js
@@ -26,10 +26,28 @@
 
             // implement our fixtures
             Object.entries(routes).forEach(([path, endpoint]) => {
-                cy.fixture(`${endpoint}.json`).as(`fixture${endpoint}`);
-                cy.route(path, `@fixture${endpoint}`).as(endpoint);
+                //cy.fixture(`${endpoint}.json`).as(`fixture${endpoint}`);
+                cy.route(path, `fixture:${endpoint}.json`).as(endpoint);
             });
         });
     };
+
+    // https://github.com/cypress-io/cypress/issues/76
+    function loadLargeFixture(url, response, method = 'GET') {
+        return cy.route({
+            url,
+            method,
+            onRequest: xhr => {
+                const originalOnLoad = xhr.xhr.onload;
+                xhr.xhr.onload = function () {
+                    Object.defineProperty(this, 'response', {
+                        writable: true
+                    });
+                    this.response = response;
+                    originalOnLoad.apply(this, xhr);
+                };
+            }
+        });
+    }
 });
 //# sourceMappingURL=router.js.map
